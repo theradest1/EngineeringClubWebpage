9e82c1c8bccdbdf27f73cca8f29a524cdfdaf5f9
TheRad1 <18326@verona.k12.wi.us>
2022-10-07 08:00:54 -0500

    lots

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+const host = "0.0.0.0";
+
+app.get("/home/", (req, res) => {
+	res.send("server up");
+});
+
+app.listen(port, host, () => {
+	console.log("Server started on port " + port);
+});
