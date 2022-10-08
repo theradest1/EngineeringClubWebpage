70aa7dbfa98ab31e7a5c8e96b1bb68d6cd2a638f
TheRadest1 <18326@verona.k12.wi.us>
2022-10-08 14:56:08 +0100

    discord bot cant pull from git while hosting website

--- DiscordBot.py
+++ DiscordBot.py
@@ -12,7 +12,7 @@ import threading
 hiddenInfoFile = open("hiddenInfo.txt")
 hiddenInfo = content = hiddenInfoFile.readlines()
 
-start_file = "/home/landonbakken/Desktop/MinecraftModThings/Paper-1.18/start.sh"
+start_file = "/home/server/EngineeringClubWebpage/StartLiveServer.sh"
 
 git_dir = "/home/server/EngineeringClubWebpage"
 g = git.cmd.Git(git_dir)
@@ -40,42 +40,22 @@ bot = commands.Bot(command_prefix="@", intents = intents)
 
 
 def start_server():
-	server = Popen(start_file, shell=True, preexec_fn=os.setsid)
+	server = Popen("live-server index.html", shell=True, preexec_fn=os.setsid)
 	#countThread = threading.Thread(target=countForServer, name="count")
 	#countThread.start(server)
 	return server
-	
+
 
 @bot.command()
 async def server(ctx, arg):
-	arg = arg.lower()	
+	arg = arg.lower()
 	if arg == "start":
 
-		""" try:"""
-		await ctx.send("Starting")
-		"""except:
-			await ctx.send("Starting server...")
-			start_server()
-			await ctx.send("The server is up")
-	elif arg == "stop":
 		try:
-			os.killpg(os.getpgid(server.pid), signal.SIGTERM)
-			await ctx.send("Server stopped")
+			await ctx.send("Starting")
+			start_server()
 		except:
-			await ctx.send("The server was not running") """
-	#else:
-		""" try:
-			if arg == "ping":
-				await ctx.send(mcserver.ping())
-			elif arg == "status":
-				status = mcserver.status()
-				if status.players.online != 1:
-					await ctx.send(f"{status.players.online} players with {status.latency} milliseconds of latency")
-				else:
-					await ctx.send(f"{status.players.online} player with {status.latency} milliseconds of latency")
-		except ConnectionRefusedError:
-			await ctx.send("The server is not up at the moment, if you want to start the server, run the command '@server start'") """
-		
+			await ctx.send("Server Start Failed (not sure why)")
 
 @bot.command()
 async def git(ctx, arg):
