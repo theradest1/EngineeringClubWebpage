7f87a48da4dfe215dcbeaa4008f4726490812c1a
TheRad1 <18326@verona.k12.wi.us>
2022-10-08 07:16:06 -0500

    added and working on discord bot

--- DiscordBot.py
+++ DiscordBot.py
@@ -9,19 +9,19 @@ import time
 import signal
 import threading
 
+hiddenInfoFile = open("hiddenInfo.txt")
+hiddenInfo = content = hiddenInfoFile.readlines()
+
 start_file = "/home/landonbakken/Desktop/MinecraftModThings/Paper-1.18/start.sh"
 
-scripts_dir = "/home/landonbakken/Desktop/MinecraftModThings/Paper-1.18/plugins/Skript/scripts"
-g = git.cmd.Git(scripts_dir)
-
-#When the ip is changed for the server, you need to edit here, the server config, and both the UDP and TCP port IPs
-mcserver = JavaServer("192.168.0.19", 25565)
+git_dir = "/home/landonbakken/Desktop/MinecraftModThings/Paper-1.18/plugins/Skript/scripts"
+g = git.cmd.Git(git_dir)
 
 bot = commands.Bot(command_prefix="@")
 
-max_on_time = 30
+#max_on_time = 30
 
-def countForServer(server):
+""" def countForServer(server):
 	global mcserver, max_on_time
 	on_time = 0
 	time.sleep(30) #wait untill the server is actually on
@@ -34,7 +34,7 @@ def countForServer(server):
 				on_time = 0
 		except:
 			return
-	os.killpg(os.getpgid(server.pid), signal.SIGTERM)
+	os.killpg(os.getpgid(server.pid), signal.SIGTERM) """
 
 
 def start_server():
@@ -46,26 +46,23 @@ def start_server():
 
 @bot.command()
 async def server(ctx, arg):
-	global mcserver, server
 	arg = arg.lower()	
 	if arg == "start":
-		try:
-			mcserver.ping()
-			await ctx.send("server is already up")
-		except:
+
+		""" try:"""
+		await ctx.send("Starting")
+		"""except:
 			await ctx.send("Starting server...")
-			server = start_server()
-			time.sleep(45)
+			start_server()
 			await ctx.send("The server is up")
 	elif arg == "stop":
 		try:
-			mcserver.ping()
 			os.killpg(os.getpgid(server.pid), signal.SIGTERM)
 			await ctx.send("Server stopped")
 		except:
-			await ctx.send("The server was not running")
-	else:
-		try:
+			await ctx.send("The server was not running") """
+	#else:
+		""" try:
 			if arg == "ping":
 				await ctx.send(mcserver.ping())
 			elif arg == "status":
@@ -75,23 +72,22 @@ async def server(ctx, arg):
 				else:
 					await ctx.send(f"{status.players.online} player with {status.latency} milliseconds of latency")
 		except ConnectionRefusedError:
-			await ctx.send("The server is not up at the moment, if you want to start the server, run the command '@server start'")
+			await ctx.send("The server is not up at the moment, if you want to start the server, run the command '@server start'") """
 		
 
 @bot.command()
 async def git(ctx, arg):
-	global mcserver
 	arg = arg.lower()
 	if arg == "pull":
 		try:
 			g.pull()
 			await ctx.send(f"Git pull successful")
 		except:
-			await ctx.send("Git was not able to do a pull request")
+			await ctx.send("Git was not able to do a pull request (not sure why)")
 
 @bot.command()
 async def die(ctx):
 	await ctx.send("Bye (:")
 	exit()
 
-bot.run('NzgzMDgxMjE1NjU3MjQ2NzMy.X8VjNg.mfWSDWwiXI6SHc2SJfQRG267UPQ')
+bot.run(hiddenInfo[0])
