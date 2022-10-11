b4d7de910ee6287e0ff12ec56e90579363025383
TheRadest1 <18326@verona.k12.wi.us>
2022-10-10 20:28:24 -0500

    A bunch of small changes

--- app.js
+++ app.js
@@ -20,6 +20,15 @@ app.get("/seaperch", (req, res) => {
 app.get("/rocketry", (req, res) => {
 	res.sendFile(__dirname + "/views/rocketry.html");
 });
+app.get("/3d-printing", (req, res) => {
+	res.sendFile(__dirname + "/views/3d-printing.html");
+});
+app.get("/synthetic-hand", (req, res) => {
+	res.sendFile(__dirname + "/views/synthetic-hand.html");
+});
+app.get("/drones", (req, res) => {
+	res.sendFile(__dirname + "/views/drones.html");
+});
 
 app.listen(port, host, () => {
 	console.log("Server started on port " + port);
